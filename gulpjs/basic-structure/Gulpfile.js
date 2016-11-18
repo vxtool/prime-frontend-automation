d5075fb662e3fc6f5637169c87e8303f7e2a1b72
Hemerson Vianna <hemerson.lourenco@gmail.com>
2016-11-18 01:18:36 -0200

    #3 [add] basic structure

---
+++ gulpjs/basic-structure/Gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+
+gulp.task('uglify', function() {
+  return gulp.src('src/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['uglify'], function () {
+
+});
---
+++ gulpjs/basic-structure/src/scripts.js
@@ -0,0 +1,5 @@
+alert('Teste 1');
+alert('Teste 2');
+alert('Teste 3');
+alert('Teste 4');
+alert('Teste 5');
